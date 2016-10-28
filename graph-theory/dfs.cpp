b4ef4fa83c189b5ff3d037597cc92dbf2f3b9926
BakhtiyarKBTU <bakhtiyarkbtu@gmail.com>
2016-10-28 13:13:17 +0600

    Update dfs.cpp

--- graph-theory/dfs.cpp
+++ graph-theory/dfs.cpp
@@ -45,6 +45,54 @@ const int MAX_LEN = 200200;
 int n, m;
 vector<pii> bridges;
 vector<int> g[MAX_LEN];
+int cnt = 1, used[MAX_LEN], c[MAX_LEN], tin[MAX_LEN], fup[MAX_LEN], timer = 1;
+
+/*dfs by Sabo */
+void dfs(int from){
+    used[from] = cnt;
+    for(int i = 0; i < sz(g[from]); i++){
+        int to = g[from][i];
+        if(used[to] != cnt) dfs(to);
+    }
+}
+
+/* find cycle */
+int cycle(int from){
+    c[from] = 1;
+    for(int i = 0; i < sz(g[from]); i++){
+        int to = g[from][i];
+        if(c[to] == 0){ 
+            if(dfs(to)) return 1;
+        }
+        else if(c[to] == 1) return 1;
+    }
+    c[from] = 2;
+    return 0;
+}
+
+/* find bridges */
+map < pair <int, int>, bool > was;
+void bridge(int from, int parent){
+    tin[from] = fup[from] = timer++;
+    for(int i = 0; i < sz(g[from]); i++){
+        int to = g[from][i];
+        if(to != parent){
+            dfs(to, from);
+            if(fup[to] == tin[to]) {
+                int x = min(to, from);
+                int y = max(to, from);
+                if(m[mp(x, y)] == 0){
+                    m[mp(x, y)] = 1;
+                    bridges.pb(mp(from, to));
+                }
+                fup[from] = min(fup[from], fup[to]);
+            }
+            else{
+                fup[from] = min(fup[from], tin[to]);
+            }
+        }
+    }
+}
 
 int main(){
     return 0;
